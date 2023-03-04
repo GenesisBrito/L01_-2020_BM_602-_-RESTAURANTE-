21eed61e61902e7f6db262cc11b4469e3c71b241
genes <genes@DESKTOP-5E44V4S>
2023-03-04 09:48:59 -0600

    Agregar archivos de proyecto.

---
+++ L01_[2020_BM_602]_[RESTAURANTE]/Models/restauranteContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using webApiPractica.Models;
+
+namespace L01__2020_BM_602___RESTAURANTE_.Models
+{
+    public class restauranteContext : DbContext
+    {
+        public restauranteContext(DbContextOptions<restauranteContext> options) : base(options)
+        {
+
+        }
+        public DbSet<restaurante> equipos { get; set; }
+    }
+}
---
+++ L01_[2020_BM_602]_[RESTAURANTE]/Properties/restaurantes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace L01__2020_BM_602___RESTAURANTE_.Properties
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class restaurantes : ControllerBase
+    {
+        public class equipos
+        {
+            [Key]
+            public int { get; set; }
+            public string nombre { get; set; }
+            public string descripcion { get; set; }
+            public int? tipo_equipo_id { get; set; }
+            public int? marca_id { get; set; }
+            public string modelo { get; set; }
+            public int? anio_compra { get; set; }
+            public decimal costo { get; set; }
+            public int? vida_util { get; set; }
+            public int? estado_equipo_id { get; set; }
+            public string estado { get; set; }
+        }
+
+    }
+}
+}
